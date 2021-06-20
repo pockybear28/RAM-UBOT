b0ff1459fe845501805fab7d14c1d14b5507377a
pockybear28 <83899890+pockybear28@users.noreply.github.com>
2021-06-20 18:21:32 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"⭐RAM-UBOT⭐ 🔥 V7.0 [AKTIF NGENTOT!!!!]")
+    f"👳SULTAN-UBOT👳 🔥 V7.0 [TELAH DIAKTIFKAN!!!!]")
 
 
 if len(argv) not in (1, 3, 4):
